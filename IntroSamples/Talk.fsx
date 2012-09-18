4212cecc8ae6f7e03a5245c637fc079e2783c97e
Rachel Reese <rreese@gmail.com>
2012-09-17 20:38:19 -0400

    Updates for the NYC Code Camp

--- IntroSamples/Talk.fsx
+++ IntroSamples/Talk.fsx
@@ -1,7 +1,6 @@
 ﻿//IMMUTABLE, TYPE INFERENCE
 //Can't mix types: 
 let n = 5
-//n = 4
 //let inverse = 1.0 / n 
 
 //automatically changes
@@ -12,7 +11,7 @@ let square y = y * y
 // LISTS
 // ANONYMOUS FUNCTIONS
 // FUNCTION COMPOSITION, PIPELINING
-let first_ten = [1..10]
+let first_ten = [1..10] 
 let evens = [2..2..10]
 
 let plus_3 x = x + 3
@@ -20,35 +19,36 @@ let list_plus_3 = List.map plus_3
 
 let filtered = List.filter (fun x -> x % 2 = 0)
 
-let result = 
-  [1..10] 
-    |> filtered 
-    |> list_plus_3
-    |> List.sum
-
+[1..10]
+  |> filtered 
+  |> list_plus_3
+  |> List.sum
+     
 let sum_evens_plus_three = 
   filtered 
     >> list_plus_3
     >> List.sum
+
 sum_evens_plus_three [1..10]
 
 
 // RECURSION, MATCHING 
 let rec factorial x = 
     match x with 
-        | 1 ->  1
+        | 0 | 1 -> 1
         | _ when x < 0 -> 0
-        | _ -> x * factorial (x - 1) 
-factorial -5
+        | _ -> x * factorial (x - 1)
+factorial 0
 
-let find_num list = 
-    let attempt = List.tryFind(fun x -> x = 4) list
+let find_num num list = 
+    let attempt = List.tryFind(fun x -> x = num) list
     match attempt with 
         | Some(num) -> printfn "Found %d!" num
         | None -> printfn "Nothing :(" 
 
-find_num [1..5]
-find_num [1..2..15]
+find_num 3 [1;3;3;5]
+find_num 5 [1..2..15]
+find_num 4 [0..5..40]
 
 
 // SEQUENCES
@@ -75,8 +75,9 @@ let euclidean = minkowski 2.0
 
 let point_1 = [0.0; 0.0]
 let point_2 = [3.0; 4.0]
-let dist_m = manhattan point_1 point_2
-let dist_e = euclidean point_1 point_2
+
+manhattan point_1 point_2
+euclidean point_1 point_2
 
 
 // UNITS OF MEASURE
@@ -86,30 +87,48 @@ let dist_e = euclidean point_1 point_2
 [<Measure>] type km // kilometres
 [<Measure>] type hr // hour
 
-let convert_km_to_v (v:float<km/hr>) = float(v) ** 0.16
-let convert_mi_to_v (v:float<mi/hr>) =  float(v) ** 0.16
-
 let WindChill_US (T:float<F>) (v:float<mi/hr>) = 
-    35.74<F> + 0.6215 * T - 35.75<F> * (convert_mi_to_v v) + 0.3965 * T * (convert_mi_to_v v)
+    35.74<F> + 0.6215 * T - 35.75<F> * float(v) ** 0.16 + 0.3965 * T * float(v) ** 0.16
 
 let WindChill_CA (T:float<C>) (v:float<km/hr>) = 
-    13.12<C> + 0.6215 * T - 11.37<C> * (convert_km_to_v v) + 0.4275 * T * (convert_km_to_v v)
+    13.12<C> + 0.6215 * T - 11.37<C> * float(v) ** 0.16 + 0.4275 * T * float(v) ** 0.16
 
 let NYCTemp = 6.0<F>
 let NYCWindSpeed = 45.2<mi/hr>
 
-let NY_WindChill = WindChill_US NYCTemp NYCWindSpeed
+WindChill_US NYCTemp NYCWindSpeed
 
 let MontrealTemp = -5.0<C>
 let MontrealWindSpeed = 25.2<km/hr>
 
-let Montreal_WindChill = WindChill_CA MontrealTemp MontrealWindSpeed
+WindChill_CA MontrealTemp MontrealWindSpeed
 
 
 // DECLARING OPERATORS, USING .NET CODE
 open System.Collections.Generic
 let (.&) left right = 
-  let cache = HashSet< 'a >(right, HashIdentity.Structural)
+  let cache = HashSet<'a>(right, HashIdentity.Structural)
   List.filter (fun n -> cache.Contains n) left
 
 [1..15] .& [0..3..25]
+["apples"; "oranges"; "pumpkins"; "pomegranates"; "kiwi"] .& ["bears"; "tigers"; "kiwi"; "lions"; "penguins"]
+
+
+//TYPE PROVIDERS
+#r "System.Runtime.Serialization"
+#r "System.ServiceModel"
+#r "FSharp.Data.TypeProviders"
+
+let currentZip = "05401"
+
+type WeatherService = Microsoft.FSharp.Data.TypeProviders.WsdlService<ServiceUri = "http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL">
+
+type forecast = WeatherService.ServiceTypes.ws.cdyne.com.WeatherWS.Forecast
+
+let weather2354 = WeatherService.GetWeatherSoap().
+let weather = WeatherService.GetWeatherSoap().GetCityWeatherByZIP(currentZip)
+
+let temp = weather.Temperature
+let description = weather.Description
+let humidity = weather.RelativeHumidity
+
---
+++ IntroSamples/weather-type-provider.fs
@@ -0,0 +1,20 @@
+﻿module weather
+
+open System.Runtime.Serialization
+open System.ServiceModel
+open Microsoft.FSharp.Data.TypeProviders
+
+#if INTERACTIVE
+#r "System.Runtime.Serialization"
+#r "System.ServiceModel"
+#r "FSharp.Data.TypeProviders"
+#endif
+
+let currentZip = "05401"
+
+type WeatherService = Microsoft.FSharp.Data.TypeProviders.WsdlService<ServiceUri = "http://wsf.cdyne.com/WeatherWS/Weather.asmx?WSDL">
+
+let weather = WeatherService.GetWeatherSoap().GetCityWeatherByZIP(currentZip)
+let temp = weather.Temperature
+let description = weather.Description
+let humidity = weather.RelativeHumidity
